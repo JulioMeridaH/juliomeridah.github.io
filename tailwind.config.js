d766049f221c88455ed0a70678117d9a82b1a53a
juliomeridah <juliomeridahernandez@gmail.com>
2024-11-18 20:27:08 -0300

    Fix purge issue in Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./*.html",          // All HTML files in the root
+    "./src/**/*.html",   // All HTML files in a 'src' folder
+    "./src/**/*.js",     // All JS files if applicable
+  ],
   theme: {
     extend: {},
   },
